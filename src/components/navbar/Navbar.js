0811ef54458521454c9445b4cdb07a21f49ba037
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-04-21 18:40:14 -0400

    Navbar v1

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+        <ul className = "Navbar">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
