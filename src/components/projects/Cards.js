91062f8e5704b414c193f89556674b67a5f4f4b5
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-04-27 01:40:46 -0400

    fixed redirect and title

--- src/components/projects/Cards.js
+++ src/components/projects/Cards.js
@@ -1,12 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import "./Cards.css"
 
 function Cards({title, siteImg,link}) {
   return (
     <div className='card'>
-        <img src={siteImg} alt="site1"/>
+        <img src={siteImg} alt={title}/>
         <h3>{title}</h3>
         <a target="_blank" rel="noreferrer" href={link}>Visit Site</a>
+       
     </div>
   )
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
         <h2>Projects I've worked on</h2>
 
         <div className="projects-grid">
-          <Cards title="Netflix Redesign" siteImg="./netflix.png" link="https://google.com"/>
+          <Cards title="Netflix Redesign" siteImg="./netflix.png" link="https://r2h-netflix.herokuapp.com/"/>
           <Cards title="Hogar" siteImg="./hogar.png" link="https://samueljr3.github.io/code4Justice/"/>
           <Cards title="Sugoi Ecommerce" siteImg="./sugoi.png" link="https://sam-sugoi-ecommerce.herokuapp.com/"/>
           <Cards title="Gateway Project" siteImg="./gateway.png" link="https://el-salv-site.samueljr3.repl.co/index.html"/>
