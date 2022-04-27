b8783eeb07959dab77aacaacf59c0fb88b23cd4a
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-04-26 23:02:56 -0400

    fixed placeholders

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -6,15 +6,15 @@ function ContactForm() {
       <form className="contact-form">
         <label>
           Name:
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="name" placeholder="name"/>
         </label>
         <label>
           Email:
-          <input type="email" placeholder="johndoe@email.com" required />
+          <input type="email" placeholder="email" required />
         </label>
         <label>
           Message:
-          <textarea type="text"></textarea>
+          <textarea type="text" placeholder="message"></textarea>
         </label>
         <button type="submit">Send</button>
       </form>
